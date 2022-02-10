5decb2e891f88a7bbcd04fc12f10ddf806ac39d8
Newjin <programmernewjin@gmail.com>
2022-02-10 18:24:47 +0530

    Add Two Number and return their Index

--- AddTwoNumFindIndex.cpp
+++ AddTwoNumFindIndex.cpp
@@ -1,30 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int *findSum(int A[], int n, int k)
+vector<int> twoSum(vector<int> &nums, int target)
 {
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            if (A[i] + A[j] == k)
-            {
-                int *arr = new int[2]{i, j};
-                return arr;
-            }
-    return NULL;
+    // Brute Force
+
+    // for (int i = 0; i < nums.size(); i++)
+    //     for (int j = i + 1; j < nums.size(); j++)
+    //         if (nums[i] + nums[j] == target)
+    //         {
+    //             vector<int> ans = {i, j};
+    //             return ans;
+    //         }
+
+    // Optimized Version
+
+    map<int, int> map;
+    vector<int> ans;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (map.count(nums[i]) != 0)
+        {
+            ans.push_back(map.at(nums[i]));
+            ans.push_back(i);
+            return ans;
+        }
+        else
+            map.insert({(target - nums[i]), i});
+    }
+    return ans;
 }
 
 int main()
 {
-    int n, k;
+    int n, k, x;
     cin >> n >> k;
-    int A[n];
+    vector<int> initalValues;
 
     for (int i = 0; i < n; i++)
-        cin >> A[i];
+    {
+        cin >> x;
+        initalValues.push_back(x);
+    }
 
-    int *ans = findSum(A, n, k);
+    vector<int> ans = twoSum(initalValues, k);
     cout << ans[0] << " " << ans[1] << endl;
-    delete[] ans;
 
     return 0;
 }
