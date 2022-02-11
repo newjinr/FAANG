dd9b66b0121aea006c85933fcef51b2eacd0911f
Newjin <programmernewjin@gmail.com>
2022-02-11 22:28:22 +0530

    Trapping Rain Water

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -45,7 +45,7 @@ int trap(vector<int> height)
         {
             if (leftMax < height[pl])
                 leftMax = height[pl];
-            else if (leftMax - height[pl] > 0)
+            else
                 units += leftMax - height[pl];
             pl++;
         }
@@ -53,7 +53,7 @@ int trap(vector<int> height)
         {
             if (rightMax < height[pr])
                 rightMax = height[pr];
-            else if (rightMax - height[pr] > 0)
+            else
                 units += rightMax - height[pr];
             pr--;
         }
